eb587b54b769108c7283be20c0170a0f8a2d2349
Yuki Saito <saitoyukisaito@gmail.com>
2020-11-07 23:01:32 -0800

    sign up page
    added all parameters and verified if they can be accessed from page. Need to write code to add information to local storage with unique key.

--- js/signup-page.js
+++ js/signup-page.js
@@ -5,15 +5,59 @@ const signupErrorMsg = document.getElementById("signup-error-msg");
 signupButton.addEventListener("click", (e) => {
     e.preventDefault();
 
+
+    // Grabs values from radio/gender choices
+    var ele = document.getElementsByClassName("gender-option");
+    let choice;
+
+    for (let i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            choice = ele[i].value;
+        }
+    }
+
+    // NOTE: Later generate unique key for each profile but for now use KEYONE
+
+    // Grabs other values and puts into userInfo object
     let userInfo = {
         username: document.getElementById("username-field").value,
         password: document.getElementById("password-field").value,
         password2: document.getElementById("password2-field").value,
+        gender: choice,
+        age: document.getElementById("age").value,
+        weight: document.getElementById("weight").value,
+        height: document.getElementById("height").value,
         question: document.getElementById("question-select").value,
-        answer: document.getElementById("answer-field").value
+        answer: document.getElementById("answer-field").value,
+
+        key: "KEYONE"
     }
 
-    for (const [key, value] of Object.entries(userInfo)) {
-        console.log(`${key}: ${value}`);
+    // Checks if any of the fields are blank
+    let valuesLog = Object.values(userInfo);
+
+    let emptyField = false;
+
+    for (let i = 0; i < valuesLog.length; i++) {
+        if (valuesLog[i] == "" || valuesLog[i] == undefined) {
+            emptyField = true;
+        }
+    }
+
+    if (emptyField) {
+        alert("You have left a field blank");
+        location.reload();
+    }
+
+
+    // Checks if the passwords match
+
+    // NOTE: can add other checks if username is already in use
+    // check if age, weight, and height are bizzare
+    // also, we can merge the error checking/alerts into one method/class
+
+    if (userInfo.password != userInfo.password2) {
+        alert("Passwords do not match");
+        location.reload();
     }
 })
