ce3a7cf2172cae2299aea4b7f9ec84df0a7c0a87
yuki_s <saitoyukisaito@gmail.com>
2020-12-04 14:03:10 -0800

    theme button
    remembers user's theme change

--- js/accessory.js
+++ js/accessory.js
@@ -1,26 +1,39 @@
-const btn = document.querySelector (".btn-toggle");
-const currentTheme = localStorage.getItem ("theme");
+const btn = document.querySelector(".btn-toggle");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == null) {
+  localStorage.setItem("theme", "dark");
+}
+
 if (currentTheme == "light") {
-  document.body.classList.add ("light-theme");
+  document.body.classList.add("light-theme");
 }
-let theme = "light";
-if (document.body.classList.contains ("light-theme")){
-  theme = "dark";
-}
-localStorage.setItem("theme", theme);
+
+
 
 function myFunction() {
   var element = document.body;
   element.classList.toggle("light-theme");
+
+  if (currentTheme == "dark") {
+    document.body.classList.add("light-theme");
+  }
+
+  let theme = "dark";
+
+  if (document.body.classList.contains("light-theme")) {
+    theme = "light";
+  }
+
+  localStorage.setItem("theme", theme);
 }
 
 function openSideMenu() {
-    document.getElementById("side-menu").style.width = "250px";
-    
+  document.getElementById("side-menu").style.width = "250px";
+
 }
 
 function closeSideMenu() {
-    document.getElementById("side-menu").style.width = "0px";
-    
-}
+  document.getElementById("side-menu").style.width = "0px";
 
+}
